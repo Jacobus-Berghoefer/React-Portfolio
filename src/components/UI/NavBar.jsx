4ec3a322c6bbf350ed232a12bbb473b350f6b998
Jacobus <jacobus.berghoefer@gmail.com>
2025-02-08 16:21:44 -0600

    updated CSS so Nav has media query for mobile

--- src/components/UI/NavBar.jsx
+++ src/components/UI/NavBar.jsx
@@ -3,16 +3,17 @@ import { NavLink } from "react-router-dom";
 export default function NavBar({ links }) {
     return (
         <nav className="text-white py-4">
-          <div className="container mx-auto flex justify-between items-center px-6">
+          {/* <div className="container mx-auto flex justify-between items-center px-6"> */}
+          <div className="container mx-auto flex flex-col items-center md:flex-row md:justify-between px-6 w-full">
             {/* Logo / Branding */}
-            <h1 className="text-2xl font-bold">
+            <h1 className="text-2xl font-bold text-center w-full md:w-auto md:text-left mb-4 md:mb-0">
               <NavLink to="/" className="hover:text-gray-400">
                 Jacobus Berghoefer
               </NavLink>
             </h1>
     
             {/* Navigation Links */}
-            <ul className="flex space-x-6">
+            <ul className="flex flex-col items-center w-full md:w-auto md:flex-row md:space-x-6 space-y-2 md:space-y-0">
               {links.map((link, index) => (
                 <li key={index}>
                   <NavLink
